e8df49c496d3a426288d0ed2b7f0759cc037ddc7
sushantguha <helloman9781@gmail.com>
2026-05-20 21:27:23 -0700

    Week6

--- week6/day1.cu
+++ week6/day1.cu
@@ -1,7 +1,7 @@
 #include <iostream>
 
 __global__ void branch() {
-    if (threadIdx.x % 2 == 0) {
+    if ((threadIdx.x / 32) % 2) {
         // printf("Even thread: %d\n", threadIdx.x);
         int a = 1;
     } else {
@@ -18,3 +18,10 @@ int main() {
     cudaDeviceSynchronize();
     return 0;
 }
+
+// WITH WARP DIVERGENCE
+//   "ID","Process ID","Process Name","Host Name","Kernel Name","Context","Stream","Block Size","Grid Size","Device","CC","Section Name","Metric Name","Metric Unit","Metric Value"                                                                                                                      
+//   "0","2583","a","127.0.0.1","branch()","1","7","(256, 1, 1)","(1, 1, 1)","0","7.5","Command line profiler metrics","dram__bytes_read.sum","byte","2,816"                                                                                                                                             
+//   "0","2583","a","127.0.0.1","branch()","1","7","(256, 1, 1)","(1, 1, 1)","0","7.5","Command line profiler metrics","dram__bytes_write.sum","byte","0"                                                                                                                                                
+//   "0","2583","a","127.0.0.1","branch()","1","7","(256, 1, 1)","(1, 1, 1)","0","7.5","Command line profiler metrics","dram__throughput.avg.pct_of_peak_sustained_elapsed","%","0.38"                                                                                                                   
+//   "0","2583","a","127.0.0.1","branch()","1","7","(256, 1, 1)","(1, 1, 1)","0","7.5","Command line profiler metrics","sm__cycles_elapsed.avg","cycle","1,362.20"  
