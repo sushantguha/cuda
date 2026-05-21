303725ee5ae8383b4fd31e91d3d2dd7a8ae4f3ed
sushantguha <helloman9781@gmail.com>
2026-05-20 21:23:16 -0700

    Week6

--- week6/day1.cu
+++ week6/day1.cu
@@ -3,11 +3,14 @@
 __global__ void branch() {
     if (threadIdx.x % 2 == 0) {
         // printf("Even thread: %d\n", threadIdx.x);
+        int a = 1;
     } else {
         // printf("Odd thread: %d\n", threadIdx.x);
+        int b = 2;
     }
     __syncwarp();
     // printf("Completed thread: %d\n", threadIdx.x);
+    int c = 3;
 }
 
 int main() {
