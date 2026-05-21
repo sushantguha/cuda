1909750b2b2ecc506f03810f78d8d802c7bf43fb
sushantguha <helloman9781@gmail.com>
2026-05-20 21:42:46 -0700

    Week6

--- week6/day1.cu
+++ week6/day1.cu
@@ -4,12 +4,11 @@ __global__ void branch(int* out) {
     int v;
     if ((threadIdx.x % 2) == 0) {
         v = 0;
-        for (int i = 0; i < 100; i++) v = v * 3 + i;
+        for (int i = 0; i < 200; i++) v = v * 3 + i;
     } else {
         v = 1;
-        for (int i = 0; i < 100; i++) v = v * 5 + i;
+        for (int i = 0; i < 800; i++) v = v * 5 + i;
     }
-    __syncwarp();
     out[blockIdx.x * blockDim.x + threadIdx.x] = v;
 }
 
